8544e26e19ccb903ebfdecd2d12e62028744f4f2
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-02-12 15:46:56 +0100

    prettier

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,10 +1,10 @@
 const messageList = {
-  400: "Bad Request",
-  401: "Unauthorized",
-  403: "Forbidden",
-  404: "Not Found",
-  409: "Conflict",
-  500: "Server Error",
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'Not Found',
+  409: 'Conflict',
+  500: 'Server Error',
 };
 
 const HttpError = (status, message = messageList[status]) => {

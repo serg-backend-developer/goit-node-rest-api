116d768ab6b3bbac13a24886725790c4004b3b47
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-03-10 16:57:59 +0100

    fin

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,76 +1,78 @@
+import gravatar from 'gravatar';
 import fs from 'fs/promises';
 import path from 'path';
 
-import { avatarStore } from '../constants/consts.js';
-import HttpError from '../helpers/HttpError.js';
-import authServices from '../services/authServices.js';
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    uploadAvatarService,
+} from '../services/authServices.js';
 
-const uploadDir = path.join(process.cwd(), 'temp');
-const storeImage = path.join(process.cwd(), 'public', avatarStore);
+import { __dirname } from '../constants/consts.js';
 
-export const register = async (req, res, next) => {
-    const result = await authServices.register(req.body);
-    res.status(201).json({
-        user: {
-            email: result.email,
-            subscription: result.subscription,
-        },
-    });
-};
+const avatarsDir = path.join(__dirname, '../public/avatar');
 
-export const login = async (req, res, next) => {
-    const result = await authServices.login(req.body);
-    res.json(result);
-};
-
-export const logout = async (req, res) => {
-    const { email } = req.user;
-    await authServices.logout(email);
-    res.status(204).send();
-};
-
-export const getCurrent = async (req, res) => {
-    res.json({
-        email: req.user.email,
-        subscription: req.user.subscription,
-    });
-};
-
-export const subscription = async (req, res) => {
-    const { email } = req.user;
-    const result = await authServices.subscription(email, req.body);
-    res.json({
-        email: result.email,
-        subscription: result.subscription,
-    });
-};
-
-export const changeAvatar = async (req, res) => {
-    const { email } = req.user;
-    if (!mimetype.includes('image')) {
-        await fs.unlink(req.file.path);
-        return next(HttpError(400, 'File format is incorrect.'));
+export async function getCurrent(req, res) {
+    const currUser = req.user;
+    if (!currUser) {
+        return res.status(401).json({ message: 'Not authorized' });
     }
-    const tempPath = path.join(uploadDir, originalname);
-    const emailString = req.user.email
-        .replace(/[^a-zA-Z0-9]/g, '')
-        .substr(0, 10);
-    const extension = path.extname(originalname);
-    const fileName = `${emailString}-${Math.round(
-        Math.random() * 1e9
-    )}${extension}`;
+    return res.status(200).json({
+        email: currUser.email,
+        subscription: currUser.subscription,
+    });
+}
 
+export async function register(req, res) {
+    const { email, password } = req.body;
+    const avatarURL = gravatar.url(email, { s: '200', d: 'retro', r: 'pg' });
+    const registUser = await registerUser(email, password, avatarURL);
+    if (registUser.error) {
+        return res
+            .status(registUser.error.status)
+            .json({ message: registUser.error.message });
+    }
+
+    res.status(201).json(registUser);
+}
+
+export async function login(req, res) {
+    const { email, password } = req.body;
+    const loginUser = await loginUser(email, password);
+    if (loginUser.error) {
+        return res
+            .status(loginUser.error.status)
+            .json({ message: loginUser.error.message });
+    }
+    res.status(200).json(loginUser);
+}
+
+export async function logout(req, res) {
+    const user = req.user;
+    const logoutUser = await logoutUser(user);
+    if (logoutUser.error) {
+        return res
+            .status(logoutUser.error.status)
+            .json({ message: logoutUser.error.message });
+    }
+    return res.status(logoutUser.status).send();
+}
+
+export const uploadAvatar = async (req, res) => {
     try {
-        const finalPath = path.join(storeImage, fileName);
-        await fs.rename(tempPath, finalPath);
-        req.file.avatarURI = `${avatarStore}/${fileName}`;
+        if (!req.file) {
+            return res.status(400).json({ error: 'No file uploaded.' });
+        }
+        const { path: tempPath, originalname } = req.file;
+        const extName = path.extname(originalname);
+        const newFilename = `${req.user.id}${extName}`;
+        const newFilePath = path.join(avatarsDir, newFilename);
+        await fs.rename(tempPath, newFilePath);
+        const avatarURL = `/avatar/${newFilename}`;
+        await uploadAvatarService(req, avatarURL);
+        res.status(200).json({ avatarURL });
     } catch (error) {
-        await fs.unlink(tempPath);
-        throw HttpError(500, error.message);
+        res.status(500).json({ error: 'Failed to upload avatar.' });
     }
-
-    const result = await authServices.changeAvatar(email, req.file);
-    res.json({
-        avatarURL: result.avatarURL,
-    });
 };

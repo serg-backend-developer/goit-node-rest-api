7240a914936b15911b406cc6310e31d43e8e03d2
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-03-10 17:07:49 +0100

    f

--- constants/consts.js
+++ constants/consts.js
@@ -2,7 +2,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-export const avatarStore = 'avatar';
+export const avatarStore = 'avatars';
 export const dirName = path.resolve();
 export const SECRET_KEY = process.env.JWT_SECRET;
 export const __filename = fileURLToPath(import.meta.url);

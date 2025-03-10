99c6ebb5c19ac5501b892f39329265debfc0017b
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-03-10 16:50:23 +0100

    update

--- constants/consts.js
+++ constants/consts.js
@@ -1,1 +1,9 @@
-export const avatarStore = 'avatars';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+export const avatarStore = 'avatar';
+export const dirName = path.resolve();
+export const SECRET_KEY = process.env.JWT_SECRET;
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);

2d9e712aa3e90cdaecfd3ac88d12f8bc7f051828
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-03-11 11:09:22 +0100

    ssl

--- db/config.js
+++ db/config.js
@@ -7,6 +7,12 @@ const sequelize = new Sequelize({
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     dialect: process.env.DATABASE_DIALECT,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
 
 try {
